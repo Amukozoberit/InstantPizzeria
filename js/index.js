dedfbe96a1f3ad76446f26b390f38b15238f2bf2
Amukozoberit <mwasheb@gmail.com>
2021-06-26 09:22:21 +0300

    read the input values

--- js/index.js
+++ js/index.js
@@ -1,5 +1,39 @@
+let inputtedTop = [];
 $(document).ready(function() {
-    $("#new-form").click(function() {
+    $("input").change(function() {
+        var inputtedSize = $('input[name="Size"]:checked').val();
+        var inputtedCrust = $('input[name="Crust"]:checked').val();
+        $('input[name="Toppings"]:checked').each(function() {
+            //console.log(this.value);
 
-    })
+            inputtedTop.push(this.value);
+
+        });
+        console.log(inputtedTop);
+        // $('input[name="Toppings"]:checked').each(function() {
+        //     console.log(this.value);
+        // });
+
+        console.log(inputtedCrust);
+        console.log(inputtedSize);
+
+    });
+
+
+
+
+
+    $("#add-form").click(function() {
+        $("form#pizzaOrder").append(
+
+
+        )
+    });
 });
+
+
+function Order(Size, Crust, Toppings) {
+    this.Size = Size;
+    this.Crust = Crust;
+    this.Toppings = Toppings;
+}
